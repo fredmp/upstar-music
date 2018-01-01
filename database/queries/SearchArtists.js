00bf4c6be576f6ef81d01e3d1de0ddd39e4ecffd
Frederico <martinsporto@gmail.com>
2018-01-01 20:13:39 -0200

    Fix search count

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -14,7 +14,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
     .skip(offset)
     .limit(limit);
   return Promise.all([
-    find, Artist.count()
+    find, Artist.count(getQuery(criteria))
   ]).then((results) => {
     return { all: results[0], count: results[1], offset, limit };
   });
